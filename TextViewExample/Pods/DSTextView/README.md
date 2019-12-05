ac5c60123b487574b5a3804b92400a362b157423
Dharmendra Sinha <dsinha6193@gmail.com>
2019-12-06 01:12:18 +0530

    Update screenshot

--- TextViewExample/Pods/DSTextView/README.md
+++
@@ -1,2 +0,0 @@
-# DSTextView
-DSTextView to get rid of placeholder and its color issue of UITextView of UIKit in swift 5.1.
--- TextViewExample/Pods/Target Support Files/DSTextView/DSTextView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DSTextView : NSObject
-@end
-@implementation PodsDummy_DSTextView
-@end
